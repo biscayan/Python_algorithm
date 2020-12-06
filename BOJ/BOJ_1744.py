88553c951f663482e682dbb0bc6255442d626e5c
biscayan <skgudwn34@gmail.com>
2020-12-06 22:25:38 +0900

    수 묶기

--- BOJ/BOJ_1744.py
+++ BOJ/BOJ_1744.py
@@ -1,4 +1,5 @@
 import sys
+from collections import deque
 
 sys.stdin = open("input.txt","r")
 
@@ -6,50 +7,53 @@ def Bind_num(num_list):
 
     ###내림차순으로 정렬
     sorted_num_list = sorted(num_list,reverse=True)
-    
+
+    ###시간복잡도 개선을 위해 리스트를 데크로 전환
+    num_deque = deque(sorted_num_list)
+
     result = 0
 
-    while sorted_num_list:
-        if len(sorted_num_list) == 1:
-            result += sorted_num_list.pop(0)
+    while num_deque:
+        if len(num_deque) == 1:
+            result += num_deque.popleft()
 
         else:
             ###1보다 클 때
-            if sorted_num_list[0]>1:
+            if num_deque[0]>1:
 
                 ###다음 값이 1보다 크면 묶음
-                if sorted_num_list[1]>1:
-                    result += (sorted_num_list.pop(0)*sorted_num_list.pop(0))
+                if num_deque[1]>1:
+                    result += (num_deque.popleft()*num_deque.popleft())
 
                 ###다음값이 1이하이면 현재값만 더함
-                elif sorted_num_list[1]<=1:
-                    result += sorted_num_list.pop(0)
+                elif num_deque[1]<=1:
+                    result += num_deque.popleft()
 
             ###1이면 현재값만 더함
-            elif sorted_num_list[0]==1:
-                result += sorted_num_list.pop(0)
+            elif num_deque[0]==1:
+                result += num_deque.popleft()
 
             ###0일 때
-            elif sorted_num_list[0]==0:
+            elif num_deque[0]==0:
 
                 ###남은 수가 홀수개면 현재값만 더함
-                if len(sorted_num_list)%2==1:
-                    result += sorted_num_list.pop(0)
+                if len(num_deque)%2==1:
+                    result += num_deque.popleft()
 
                 ###남은 수가 짝수개면 묶음
                 else:
-                    result += (sorted_num_list.pop(0)*sorted_num_list.pop(0))
+                    result += (num_deque.popleft()*num_deque.popleft())
 
             ###음수일 때
-            elif sorted_num_list[0]<0:
+            elif num_deque[0]<0:
 
                 ###남은 수가 홀수개면 현재값만 더함
-                if len(sorted_num_list)%2==1:
-                    result += sorted_num_list.pop(0)
+                if len(num_deque)%2==1:
+                    result += num_deque.popleft()
 
                 ###남은 수가 짝수개면 묶음
                 else:
-                    result += (sorted_num_list.pop(0)*sorted_num_list.pop(0))
+                    result += (num_deque.popleft()*num_deque.popleft())
 
     print(result)
 
