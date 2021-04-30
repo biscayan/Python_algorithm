5723c2d4849690a4e16ff2e46ece3ea7422f9e7a
biscayan <skgudwn34@gmail.com>
2021-04-30 22:14:54 +0900

    컨베이어 벨트 위의 로봇

---
+++ Algorithm_problems/BOJ/구현/BOJ_20055.py
@@ -0,0 +1,45 @@
+import sys
+from collections import deque
+
+sys.stdin = open("input.txt", "r")
+
+def rotation(belt, robots):
+
+    step = 0
+
+    while True:
+
+        step += 1
+
+        ### 1. 벨트 회전
+        belt.appendleft(belt.pop())
+        robots.pop()
+        robots.appendleft(0)
+        if robots[-1] == 1:
+            robots[-1] = 0
+
+        ### 2. 로봇 이동
+        for i in range(N-2,-1,-1):
+            if robots[i] == 1:
+                if robots[i+1] != 1 and belt[i+1] >= 1:
+                    robots[i+1] = 1
+                    robots[i] = 0
+                    belt[i+1] -= 1
+
+        ### 3. 로봇 올리기
+        if belt[0] != 0:
+            robots[0] = 1
+            belt[0] -= 1
+
+        ### 4. 내구도가 0인 칸이 K개 이상이면 종료
+        if belt.count(0) >= K:
+            return step
+
+
+if __name__ == '__main__':
+
+    N, K = map(int, sys.stdin.readline().split())
+    belt = deque(list(map(int, sys.stdin.readline().split())))
+    robots = deque([0 for _ in range(N)])
+
+    print(rotation(belt, robots))
