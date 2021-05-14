ec7d12822d59c4b7f8bc84412584065a50b66298
biscayan <skgudwn34@gmail.com>
2021-05-14 11:43:55 +0900

    chapter 8 연결리스트

---
+++ Python_algorithm_interview/Chapter8/leetcode_234.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        
+        queue = deque()
+        
+        if not head:
+            return True
+        
+        node = head
+        
+        while node is not None:
+            queue.append(node.val)
+            node = node.next
+            
+        while len(queue) > 1:
+            if queue.popleft() != queue.pop():
+                return False
+        return True
