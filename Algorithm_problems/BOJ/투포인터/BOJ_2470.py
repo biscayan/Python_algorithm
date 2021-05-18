beccb461f5aa78dcec9cd148c59248e74e7c15cf
biscayan <skgudwn34@gmail.com>
2021-05-18 20:42:01 +0900

    두 용액

---
+++ Algorithm_problems/BOJ/투포인터/BOJ_2470.py
@@ -0,0 +1,37 @@
+import sys
+
+sys.stdin = open("input.txt", "r")
+
+def two_liquid(liquid_list):
+
+    # The largeset possible value
+    value = 2e9
+
+    l_idx, r_idx = 0, N-1
+
+    while l_idx < r_idx:
+
+        mixed = liquid_list[l_idx] + liquid_list[r_idx]
+
+        if abs(mixed) < value:
+            result_list = [liquid_list[l_idx], liquid_list[r_idx]]
+            value = abs(mixed)
+
+        if mixed == 0:
+            break
+        elif mixed < 0 : 
+            l_idx += 1
+        else:
+            r_idx -= 1
+
+    print(result_list[0], result_list[1])
+
+    
+if __name__ == '__main__':
+
+    N = int(sys.stdin.readline())
+    liquid_list = list(map(int,sys.stdin.readline().split()))
+
+    liquid_list.sort()
+
+    two_liquid(liquid_list)
