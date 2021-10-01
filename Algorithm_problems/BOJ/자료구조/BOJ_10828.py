5ff3e894637aada859fe869a95b71331dc54b079
biscayan <skgudwn34@gmail.com>
2021-10-01 23:02:49 +0900

    boj 스택

---
+++ Algorithm_problems/BOJ/자료구조/BOJ_10828.py
@@ -0,0 +1,30 @@
+import sys
+sys.stdin = open("input.txt", "r")
+
+N = int(sys.stdin.readline())
+stack = []
+for _ in range(N):
+    command = list(sys.stdin.readline().split())
+    if command[0] == 'push':
+        stack.append(int(command[1]))
+
+    elif command[0] == 'pop':
+        if stack:
+            print(stack.pop())
+        else:
+            print(-1)
+
+    elif command[0] == 'size':
+        print(len(stack))
+
+    elif command[0] == 'empty':
+        if stack:
+            print(0)
+        else:
+            print(1)
+            
+    elif command[0] == 'top':
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
